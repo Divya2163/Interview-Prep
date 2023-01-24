8acc2f613dd5ec9969aad2e3578d81c7f3dff598
Divya Mohan <divyamohan210603@gmail.com>
2023-01-24 06:52:05 +0530

    Time: 133 ms (40.08%), Space: 17.2 MB (78.04%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        h = {}
+        for word in strs:
+            sortedWord = ''.join(sorted(word))
+            if sortedWord not in h:
+                h[sortedWord] = [word]
+            else:
+                h[sortedWord].append(word)
+        res = []
+        for value in h.values():
+            res.append(value)
+        return res
