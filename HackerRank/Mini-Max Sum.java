46cc97a890b0d7089c073671d5f0971fa64478af
Divya Mohan <divyamohan210603@gmail.com>
2023-04-19 20:53:40 +0530

    Create Mini-Max Sum.java

---
+++ HackerRank/Mini-Max Sum.java
@@ -0,0 +1,58 @@
+/*Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+Example
+arr=[1,3,5,7,9]
+The minimum sum is 1+3+5+7=16 and the maximum sum is 3+5+7+9=24. The function prints
+16 24*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+    // Write your code here
+    long sum = 0;
+    long minsum, maxsum;
+    long min, max;
+    for(Integer number:arr){ 
+        sum = sum + number;
+    }  
+    min = Collections.min(arr);
+    max = Collections.max(arr);
+    minsum = sum - max;
+    maxsum = sum - min;
+    System.out.print(minsum+" ");
+    System.out.print(maxsum);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
